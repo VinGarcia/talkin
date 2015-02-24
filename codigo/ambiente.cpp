3d877da02fb9271478aadb5396c83f9dced37871
Vinícius Garcia <vingarcia00@gmail.com>
2015-02-24 19:55:47 -0300

    - Move the main() function from ambiente.cpp to main.cpp. - Change output binary name for the test cases in Makefile.

--- codigo/ambiente.cpp
+++ codigo/ambiente.cpp
@@ -1230,30 +1230,3 @@ int main(int argc, char* argv[])
 // */
 
 /* * * * * END TEST cContexto * * * * */
-
-// /*
-int main(int argc, char* argv[])
-{
-  try{
-    // Construir:
-    inicializa_banco();
-  
-    // Carregar drivers:
-    drivers["stdout"] = new driver_stdout();
-    drivers["talkin"] = new driver_talkin();
-    
-    // Carrego as instruções 
-    ifstream file("vocabulario.talk");
-    if(file.is_open()) readVocab(file);
-    else cout << "Vocabulário não encontrado!\n";
-    
-    // Chamar modulo de interação com o usuário:
-    interface();
-    
-  } catch(const char* c) { cout << c << endl; }
-  
-  cout << "bye!" << endl;
-  
-  return 0;
-}
-// */
--- codigo/main.cpp
+++ codigo/main.cpp
@@ -1,18 +1,33 @@
-// #include <iostream>
-// #include "padrao.hpp"
+#include <iostream>
+#include <fstream>
+#include "ambiente.hpp"
 
-// using namespace std;
-// using namespace pMatch;
+using namespace std;
+using namespace ambiente;
 
-// Atualmente o main está no final do arquivo ambiente.cpp
-// int main(int argc, char* argv[])
-// {
-//   string entrada;
+// /*
+int main(int argc, char* argv[])
+{
+  try{
+    // Construir:
+    inicializa_banco();
   
-//   while(1)
-//   {
-//     cin >> entrada;
-//     if(entrada=="exit") return 0;
-//     cout << arrayClass(entrada).str() << endl;
-//   }
-// }
+    // Carregar drivers:
+    drivers["stdout"] = new driver_stdout();
+    drivers["talkin"] = new driver_talkin();
+    
+    // Carrego as instruções 
+    std::ifstream file("vocabulario.talk");
+    if(file.is_open()) readVocab(file);
+    else cout << "Vocabulário não encontrado!\n";
+    
+    // Chamar modulo de interação com o usuário:
+    interface();
+    
+  } catch(const char* c) { cout << c << endl; }
+  
+  cout << "bye!" << endl;
+  
+  return 0;
+}
+// */
