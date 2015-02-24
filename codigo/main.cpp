8a2b1d9998f90bbc05a7cd89e1bbf673601b0d44
Vinícius Garcia <vingarcia00@gmail.com>
2015-02-24 16:37:01 -0300

    - Comment the main.cpp code since it is not being used.

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -1,17 +1,18 @@
-#include <iostream>
-#include "padrao.hpp"
+// #include <iostream>
+// #include "padrao.hpp"
 
-using namespace std;
-using namespace pMatch;
+// using namespace std;
+// using namespace pMatch;
 
-int main(int argc, char* argv[])
-{
-  string entrada;
+// Atualmente o main está no final do arquivo ambiente.cpp
+// int main(int argc, char* argv[])
+// {
+//   string entrada;
   
-  while(1)
-  {
-    cin >> entrada;
-    if(entrada=="exit") return 0;
-    cout << arrayClass(entrada).str() << endl;
-  }
-}
+//   while(1)
+//   {
+//     cin >> entrada;
+//     if(entrada=="exit") return 0;
+//     cout << arrayClass(entrada).str() << endl;
+//   }
+// }
