d648f9ee181aa9086cd554310078f0869ba9020a
Vinícius Garcia <vingarcia00@gmail.com>
2015-02-24 19:55:47 -0300

    - Move the main() function from ambiente.cpp to main.cpp - Change output binary name for the test cases in Makefile

--- codigo/ambiente.hpp
+++ codigo/ambiente.hpp
@@ -6,6 +6,17 @@
 #include "banco.hpp"
 #include "vars.hpp"
 
+// TODO: These 3 functions below should be moved to main.cpp
+// or to a new library file.
+
+// Used to read a .talk file and add it to instruction pool.
+void readVocab(std::ifstream& file);
+
+// An interactive loop for the end user.
+void interface();
+
+void inicializa_banco();
+
 namespace ambiente
 {
   // Variáveis globais
